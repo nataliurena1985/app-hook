02a10253ae6bf3782a88e3fd147ce06a9fbb3c6f
claudia <claudianataliurena@gmail.com>
2024-05-01 10:28:02 -0300

    creando una funcion como hook para incremetar de a 2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { CounterWithCustomHook } from "./components/01-useState/CounterWithCustomHook";
+//import { CounterApp } from "./components/01-useState/CounterApp";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const divRoot = document.querySelector("#root");
+
+ReactDOM.render(<CounterWithCustomHook />, divRoot);
