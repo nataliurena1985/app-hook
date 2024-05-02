95e53d9cf53a19457747ee7b2db4a9214223204a
claudia <claudianataliurena@gmail.com>
2024-05-02 11:38:19 -0300

    subiendo memo

--- src/hooks/useCouter.js
+++ src/hooks/useCouter.js
@@ -1,18 +1,18 @@
 import { useState } from "react";
 
-export const useCouter = (initialState = 100) => {
+export const useCouter = (initialState = 10) => {
   const [counter, setCounter] = useState(initialState); // 10
 
   const reset = () => {
     setCounter(initialState);
   };
 
-  const increment = (factor) => {
-    setCounter(counter + factor);
+  const increment = () => {
+    setCounter(counter + 1);
   };
 
-  const decrement = (factor) => {
-    setCounter(counter - factor);
+  const decrement = () => {
+    setCounter(counter - 1);
   };
 
   return {
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { CounterWithCustomHook } from "./components/01-useState/CounterWithCustomHook";
+import { Memorize } from "./components/06-memos/Memorize";
+//import { Layout } from "./components/05-useLayoutEffect/Layout";
+//import { RealExampleRef } from "./components/04-useRef/RealExampleRef";
+//import { FocusScreen } from "./components/04-useRef/FocusScreen";
+//import { SimpleForm } from "./components/02-useEffect/SimpleForm";
+//import { FormWithCustomHook } from "./components/02-useEffect/FormWithCustomHook";
+//import { MultipleCustomHooks } from "./components/03-examples/MultipleCustomHooks";
+//import { CounterWithCustomHook } from "./components/01-useState/CounterWithCustomHook";
 //import { CounterApp } from "./components/01-useState/CounterApp";
 
 
 
 const divRoot = document.querySelector("#root");
 
-ReactDOM.render(<CounterWithCustomHook />, divRoot);
+ReactDOM.render(<Memorize />, divRoot);
